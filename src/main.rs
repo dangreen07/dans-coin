1a01d02ec3722b62f2a4656da937105727aa761d
Daniel Green <dangreen07@outlook.com>
2026-05-23 11:07:50 +0100

    Removed commented out code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,33 +19,6 @@ pub mod peers;
 pub mod transactions;
 pub mod wallet;
 
-// fn main() {
-//     let transaction = Transaction::create_transaction(
-//         public_key.to_bytes(),
-//         10.0,
-//         0.0,
-//         vec![Recipient::new(public_key.to_bytes(), 10.0)],
-//         secret_key,
-//     );
-//     let mut genesis_block = blockchain::Block::new(vec![transaction], [0; 64], 0, 0);
-//     genesis_block.mine();
-
-//     let blockchain = BlockChain::new();
-//     let block_id = genesis_block.hashable_block.block.id;
-//     let genesis_block = match blockchain.add_block(genesis_block) {
-//         Ok(genesis_block) => genesis_block,
-//         Err(_) => match blockchain.get_block(block_id) {
-//             Ok(genesis_block) => genesis_block,
-//             Err(_) => {
-//                 println!("Error adding genesis block");
-//                 return;
-//             }
-//         },
-//     };
-//     println!("Genesis block hash: {}", hex::encode(genesis_block.hash));
-//     println!("Blockchain length: {}", blockchain.length());
-// }
-
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
